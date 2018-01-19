c26d3c59ead28e90632d53eb2ba2d67e9dee51cc
ammar falmban <devAspirant@gmail.com>
2018-01-19 18:50:33 +0300

    adding the code

---
+++ app/src/main/java/com/example/ammarfalmban/understand_json/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.ammarfalmban.understand_json;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Define UI into Java File
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // set the java with xml
+        mTextViewResult = findViewById(R.id.text_view_result);
+
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        // send a request
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse(){
+        String url = "https://api.myjson.com/bins/kp9wz";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("employees"); // define the Root
+
+                    for (int i = 0;i<jsonArray.length();i++){
+                        JSONObject emplyees = jsonArray.getJSONObject(i);
+
+                        String firstName = emplyees.getString("firstname"); // get first name
+                        int age = emplyees.getInt("age"); // get age
+                        String mail = emplyees.getString("mail"); // get mail
+
+                        // show the empolyess information into the textView
+                        mTextViewResult.append(firstName + ", " + String.valueOf(age) + ", " + mail + "\n\n");
+                    }
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+}
